f0a633de2d7195e18fdb1df0b13c956a040aaf09
Aziz Thiam <thiam.demba_aziz@courrier.uqam.ca>
2024-05-09 23:47:22 +0000

    Commit Method Hello World

---
+++ Essai2/src/essai2/Essai2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package essai2;
+
+/**
+ *
+ * @author azizt
+ */
+public class Essai2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonsoir Mec");
+    }
+    
+}
